8cb94a58a42026b63690fcf9658717606e7251b7
dimiosis <32837736+dimiosis@users.noreply.github.com>
2022-08-01 16:32:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World, from Dmitriy!');
+  res.end('Hello World, from Dmitriy !');
 }).listen(8080);
