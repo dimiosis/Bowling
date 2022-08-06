3d8c38c88531903e9eac52a229f295736c9a7e15
dimiosis <32837736+dimiosis@users.noreply.github.com>
2022-08-06 14:43:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>Hello from Dmitriy!</font><br><b>App Version 1.4</b>";
+  const message="<font color='blue'>Hello from Dmitriy!</font><br><b>App Version 1.5</b>";
   res.status(200).send(message);
 };
