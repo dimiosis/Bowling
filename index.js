33333efefe32fe04b31862be097d52fbe89764ae
Dmitriy Medvedev <dmitriym@sela.co.il>
2022-08-08 19:46:09 +0300

    c

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>Hello from Dmitriy!</font><br><b>App Version 2.5.3</b>";
+  const message="<font color='blue'>Hello from Dmitriy! FINISH</font><br><b>App Version 3.0.0</b>";
   res.status(200).send(message);
 };
